f8d70562bb19b6ee2a4ef7251926e42ac0aab98d
quantumferret <saberdin@mac.com>
2021-06-21 02:46:57 +0200

    Reformatted test progress output.

--- Tests/TestRotations.cs
+++ Tests/TestRotations.cs
@@ -12,32 +12,40 @@ namespace RbTree
 
         [Test]
         public void TestLeftRotationOnRoot() {
+            TestContext.Progress.WriteLine("Test left rotation centered on root:");
             tree.Root = new RbTree<int>.Node(5, tree.Nil) { Color = RbTree<int>.Node.ColorEnum.Black };
             tree.Root.Right = new RbTree<int>.Node(10, tree.Root) { Color = RbTree<int>.Node.ColorEnum.Red };
             
             Assert.AreEqual(5, tree.Root.Key);
             Assert.AreEqual(10, tree.Root.Right.Key);
             Assert.AreEqual(RbTree<int>.Node.Leaf(), tree.Root.Left);
-            TestContext.Progress.WriteLine($"Root: {tree.Root}, Right: {tree.Root.Right}, Left: {tree.Root.Left}");
+            TestContext.Progress.WriteLine("    Before rotation:");
+            TestContext.Progress.WriteLine($"        Root: {tree.Root}, Right: {tree.Root.Right}, Left: {tree.Root.Left}");
 
             tree.LeftRotate(tree.Root);
-            TestContext.Progress.WriteLine($"Root: {tree.Root}, Right: {tree.Root.Right}, Left: {tree.Root.Left}");
+            TestContext.Progress.WriteLine("    After rotation:");
+            TestContext.Progress.WriteLine($"        Root: {tree.Root}, Right: {tree.Root.Right}, Left: {tree.Root.Left}");
             Assert.AreEqual(10, tree.Root.Key);
             Assert.AreEqual(5, tree.Root.Left.Key);
         }
 
         [Test]
         public void TestRightRotationOnRoot() {
+            TestContext.Progress.WriteLine("Test right rotation centered on root:");
+
             tree.Root = new RbTree<int>.Node(10, tree.Nil) { Color = RbTree<int>.Node.ColorEnum.Black };
             tree.Root.Left = new RbTree<int>.Node(5, tree.Root) { Color = RbTree<int>.Node.ColorEnum.Red };
             
             Assert.AreEqual(10, tree.Root.Key);
             Assert.AreEqual(5, tree.Root.Left.Key);
             Assert.AreEqual(RbTree<int>.Node.Leaf(), tree.Root.Right);
-            TestContext.Progress.WriteLine($"Root: {tree.Root}, Right: {tree.Root.Right}, Left: {tree.Root.Left}");
+            TestContext.Progress.WriteLine("    Before rotation:");
+            TestContext.Progress.WriteLine($"        Root: {tree.Root}, Right: {tree.Root.Right}, Left: {tree.Root.Left}");
 
             tree.RightRotate(tree.Root);
-            TestContext.Progress.WriteLine($"Root: {tree.Root}, Right: {tree.Root.Right}, Left: {tree.Root.Left}");
+            TestContext.Progress.WriteLine("    After rotation:");
+
+            TestContext.Progress.WriteLine($"        Root: {tree.Root}, Right: {tree.Root.Right}, Left: {tree.Root.Left}");
             Assert.AreEqual(5, tree.Root.Key);
             Assert.AreEqual(10, tree.Root.Right.Key);
         }
